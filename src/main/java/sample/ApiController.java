e980d675f0fe1f57856ea5c71b2b929c013e5c25
HIeu <hieu0985792248@gmail.com>
2020-10-28 16:54:47 +0700

    internet connection check

--- src/main/java/sample/ApiController.java
+++ src/main/java/sample/ApiController.java
@@ -2,11 +2,15 @@ package sample;
 
 import com.darkprograms.speech.translator.GoogleTranslate;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class ApiController {
@@ -16,16 +20,43 @@ public class ApiController {
     @FXML
     WebView target;
 
+    public boolean network(){
+        try{
+            final URL url= new URL("https://www.google.com");
+            final URLConnection conn =url.openConnection();
+            conn.connect();
+            conn.getInputStream().close();
+            return true;
+        }catch (MalformedURLException e){
+            throw new RuntimeException(e);
+        }catch (IOException e){
+            return false;
+        }
+    }
+
     public void trans() throws IOException {
-        String w=word.getText();
-        WebEngine e=target.getEngine();
-        if(!w.equals("")){
-            String v= GoogleTranslate.translate("vi",w);
-            e.loadContent(v);
+        if(network()){
+            String w=word.getText();
+            WebEngine e=target.getEngine();
+            if(!w.equals("")){
+                String v= GoogleTranslate.translate("vi",w);
+                e.loadContent(v);
+            }
+            else {
+                e.loadContent("");
+            }
         }
         else {
-            e.loadContent("");
+            show_mess();
         }
+    }
+
+    public void show_mess(){
+        Alert alert =new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("Failed");
+        alert.setContentText("No internet connection");
+        alert.showAndWait();
     }
 
     public void close(){
